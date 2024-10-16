6eaaff3054134d155ea9716900b5bc287a95159c
Sebastian <sebastianleonca1@gmail.com>
2024-10-16 16:28:02 -0500

    Agregar archivos de proyecto.

---
+++ Data/JobOffer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parcial202419200088.Data;
+
+public partial class JobOffer
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Salary { get; set; } = null!;
+
+    public string Location { get; set; } = null!;
+}
---
+++ Data/Parcial202419200088DbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial202419200088.Data;
+
+public partial class Parcial202419200088DbContext : DbContext
+{
+    public Parcial202419200088DbContext()
+    {
+    }
+
+    public Parcial202419200088DbContext(DbContextOptions<Parcial202419200088DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<JobOffer> JobOffer { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=Parcial202419200088;Integrated Security=true;TrustServerCertificate=True");
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
